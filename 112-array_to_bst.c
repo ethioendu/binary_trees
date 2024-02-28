a0886d59d7c5dcb6e38365da0704a08e2b10a784
ethioenud <ethioendu@git.com>
2024-02-28 09:23:40 -0800

    122-array_to_avl.c

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -8,13 +8,38 @@
  *
  * Return: a pointer to the root node of the created BST, or NULL on failure.
  */
-bst_t *array_to_bst(int *array, size_t size)
+#include "binary_trees.h"
+
+/**
+ * array_to_avl - turns an array to a avl tree
+ * @array: array to turns to AVL tree
+ * @size: size of array
+ * Return: AVL tree from array
+ */
+avl_t *array_to_avl(int *array, size_t size)
 {
-	bst_t *base = NULL;
-	unsigned int j;
+	size_t i, j = 0;
+	avl_t *root;
 
-	for (j = 0; j < size; j++)
-		bst_insert(&base, array[j]);
-
-	return (base);
+	root = NULL;
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			if (array[j] == array[i])
+				break;
+		}
+		if (j == i)
+		{
+			if (avl_insert(&root, array[i]) == NULL)
+			{
+				return (NULL);
+			}
+		}
+	}
+	return (root);
 }
